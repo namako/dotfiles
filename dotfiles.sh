c846feb2619ce892a24b1072ee09cc2b62107821
Takuya Usami <namakotti@gmail.com>
2026-08-17 22:28:01 +0900

    some updates

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,9 +5,6 @@ ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 
-# alacritty
-ln -s ~/dotfiles/.config/alacritty/ ~/.config/alacritty
-
 # vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 
