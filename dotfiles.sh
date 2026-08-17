674ecf95b7fce8c646dba93d1c89bc562e237ad9
Takuya Usami <namakotti@gmail.com>
2026-08-17 22:32:02 +0900

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,7 +7,12 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 
 # vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+# nvim
+ln -s ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
 
 # tmux
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-
+# Ghostty
+ln -s ~/dotfiles/.config/ghostty/config ~/.config/ghostty/config
+# herdr
+ln -s ~/dotfiles/.config/herdr/config.toml ~/.config/herdr/config.toml
